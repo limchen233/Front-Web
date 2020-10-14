a99be5fe399841c0fbc5edf36f7899fc3eef4890
limchen233 <chenlim233@163.com>
2020-10-14 17:33:21 +0800

    update

--- 15 javascript基础知识回顾/01 语言基础/01 var关键字/demoVar.js
+++ 15 javascript基础知识回顾/01 语言基础/01 var关键字/demoVar.js
@@ -6,3 +6,10 @@ function test(){
 test()
 
 console.log(message) // 报错，ReferenceError: message is not defined 因为test()函数执行完后，局部变量被销毁了
+
+function test1(){
+  message = 'hello'
+  console.log(message) // hello
+}
+test()
+console.log(messsage) // hello
